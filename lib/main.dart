83a7783985226cc7e6ca5ec13c32634838515b7f
Almatov Azamat <almatovalshin@gmail.com>
2023-01-09 23:42:43 +0500

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maplibre_gl/mapbox_gl.dart';
+import 'package:three_dart/three_dart.dart' as three;
 
 void main() => runApp(
       const MaterialApp(
@@ -17,8 +18,27 @@ class FullMap extends StatefulWidget {
 class FullMapState extends State<FullMap> {
   late MaplibreMapController controller;
 
-  _onMapCreated(MaplibreMapController controller) {
+  _onMapCreated(MaplibreMapController controller) async {
     this.controller = controller;
+
+    three.PerspectiveCamera camera = three.PerspectiveCamera(40, 1, 0.1, 10);
+    camera.position.z = 3;
+
+    three.Scene scene = three.Scene();
+    camera.lookAt(scene.position);
+
+    scene.background = three.Color(1.0, 1.0, 1.0);
+    scene.add(three.AmbientLight(0x222244, null));
+
+    var geometryCylinder = three.CylinderGeometry(0.5, 0.5, 1, 32);
+    var materialCylinder = three.MeshPhongMaterial({"color": 0xff0000});
+
+    three.Mesh mesh = three.Mesh(geometryCylinder, materialCylinder);
+    scene.add(mesh);
+
+    var object = LayerProperties().add();
+
+    controller.addLayer('custom', '3d-model', mesh);
   }
 
   @override
